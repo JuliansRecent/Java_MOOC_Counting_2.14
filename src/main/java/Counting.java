b318873aeabe4e5edbb6cc6292c5960de043871f
JuliansRecent <juliansrecent@gmail.com>
2023-03-11 11:19:35 -0800

    Module complete. Create a terminal based application that counts up the the number given by the user using a for loop.

--- src/main/java/Counting.java
+++ src/main/java/Counting.java
@@ -6,5 +6,12 @@ public class Counting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // prompt the user for an input
+        int number = Integer.valueOf(scanner.nextLine());
+
+        // in a loop, print all the numbers counting up to the number given then exit
+        for (int i = 0; i <= number; i++) {
+            System.out.println(i);
+        }
     }
 }
